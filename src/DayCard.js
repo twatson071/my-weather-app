0a44f7aba248df1caa9c86faad932c9e6c54926f
Thomas Watson <twatson071@gmail.com>
2019-12-20 13:04:06 -0500

    initial commit of weather app

---
+++ src/DayCard.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import apiConfig from './apiKeys'
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardBody
+} from "shards-react";
+import moment from 'moment'
+
+const DayCard = ({ reading }) => {
+    console.log(reading);
+    let newDate = new Date();
+    const weekday = reading.dt * 1000;
+    newDate.setTime(weekday);
+    const imgURL = `owf owf-${reading.weather.id} owf-5x`
+    fetch(`https://api.unsplash.com/search/photos/?client_id=${apiConfig.unplashedkey}&per_page=500&query=${reading.weather[0].description}`)
+    .then(res => res.json())
+    .then(data => {
+        const pagesTotal = data.total_pages;
+        const imgUrl = data.results[Math.floor(Math.random()*data.results.length)].urls.full;
+        document.body.style.backgroundImage = `url(${imgUrl})`;
+        fetch(`https://api.unsplash.com/search/photos/?client_id=${apiConfig.unplashedkey}&per_page=500&query=${reading.weather[0].description}&page=${getRandomPage(pagesTotal)}`)
+        .then(res2 => res2.json())
+        .then(data2 => {
+        if(data2){
+        const imgUrl = data2.results[Math.floor(Math.random()*data.results.length)].urls.full;
+        document.querySelector('.App').style.backgroundImage = `url(${imgUrl})`;
+        }
+    })
+    });
+    let getRandomPage = (pagesTotal) =>
+    {
+        return(Math.floor(Math.random()*pagesTotal))
+    }
+    return (
+        <Card id="weather" style={{ width: "100%", margin: "10px" }}>
+            <CardHeader>{moment(newDate).format("dddd").toLowerCase()}</CardHeader>
+            <i className={imgURL}></i>
+            <CardBody>
+                <CardTitle>{Math.round(reading.main.temp)}°F</CardTitle>
+                <p>{reading.weather[0].description}</p>
+            </CardBody>
+        </Card>
+    )
+}
+
+export default DayCard;
---
+++ src/WeekContainer.js
@@ -0,0 +1,48 @@
+
+import React from 'react';
+import apiConfig from './apiKeys';
+import DayCard from './DayCard';
+
+class WeekContainer extends React.Component {
+  state = {
+    fullData: [],
+    lat: null,
+    lng: null
+  }
+
+  render() {
+    if (this.state.fullData.length > 0) {
+      const reading = this.state.fullData[0];
+      return (
+        <div className="flex">
+          <DayCard reading={reading} key={0} />
+        </div>
+      )
+    }
+    else {
+      return (
+        <div className="flex"></div>
+      )
+    }
+  }
+  componentDidMount = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({ lat: position.coords.latitude, lng: position.coords.longitude });
+      const weatherURL =
+      `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lng}&units=imperial&APPID=${apiConfig.owmkey}`
+      fetch(weatherURL)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.data = [data];
+        this.setState({
+          fullData: this.data
+        })
+      })
+    }, err => console.log(err)
+    );
+
+  }
+}
+
+export default WeekContainer
